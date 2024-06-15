0c54e4cfdd79af3183bd16ab5f45cb8ad8ce9b12
Jake McGrath <thejakezilla80@gmail.com>
2024-06-15 18:55:16 -0500

    Updated the skeleton of the project. Added dependencies. Project requires openssl.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use log::info;
+use dotenv::dotenv;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CTA_API_KEY: String = std::env::var("CTA_API_KEY").expect("CTA_API_KEY must be set");
+    static ref CTA_BASE_URL: &'static str = "lapi.transitchicago.com/api/1.0/ttarrivals.aspx";
+}
 
 
+#[tokio::main]
 
-fn main() {
-    println!("Hello, world!");
+/// Loosely based on the following project https://www.youtube.com/watch?v=5bnAVNNSK0w.
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+
+    // TODO: instantiate an HTTP request client
+    info!("Instantiating reqwest client");
+    let client = reqwest::Client::new();
+
+    // TODO: authenticate with the API
+    info!("Authenticating with CTA API");
+
+    // TODO: Call to get latest info
+
+
+    Ok(())
 }
